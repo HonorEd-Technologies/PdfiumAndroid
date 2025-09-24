a217103a27eaf3d3f83669702a131a99a64fdb4e
Joel Chinchilla <jchinchilla@honor.education>
2025-09-23 20:07:54 -0600

    removing x86 support

--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -2,9 +2,7 @@ APP_STL := c++_shared
 APP_CPPFLAGS += -fexceptions
 
 #For ANativeWindow support
-APP_PLATFORM = android-19
+APP_PLATFORM = android-21
 
 APP_ABI :=  armeabi-v7a \
-            arm64-v8a \
-            x86 \
-            x86_64
+            arm64-v8a
