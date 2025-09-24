eba885115917db193df915ddcc5c562122fd1e81
Joel Chinchilla <jchinchilla@honor.education>
2025-09-23 20:42:16 -0600

    More fixes

--- src/main/jni/Android.mk
+++ src/main/jni/Android.mk
@@ -4,6 +4,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := pdfium
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../jniLibs/$(TARGET_ARCH_ABI)/libpdfium.so
+# Add 16KB page size support
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 include $(PREBUILT_SHARED_LIBRARY)
 
 # Main JNI library (your bridge code)
@@ -14,6 +16,8 @@ LOCAL_CFLAGS += -DHAVE_PTHREADS
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_SHARED_LIBRARIES += pdfium
 LOCAL_LDLIBS += -llog -landroid -ljnigraphics
+# Add 16KB page size support for the JNI bridge
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384
 
 LOCAL_SRC_FILES := $(LOCAL_PATH)/src/mainJNILib.cpp
 
--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -4,5 +4,8 @@ APP_CPPFLAGS += -fexceptions
 #For ANativeWindow support
 APP_PLATFORM = android-21
 
+# Add 16KB page size support globally
+APP_LDFLAGS += -Wl,-z,max-page-size=16384
+
 APP_ABI :=  armeabi-v7a \
             arm64-v8a
